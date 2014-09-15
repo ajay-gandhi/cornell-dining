3b7370988d21e10d138a8ed953660d29f879ea5b
Ajay Gandhi <aag255@cornell.edu>
2014-09-15 16:12:50 -0400

    Move styles to CSS stylesheet

--- index.php
+++ index.php
@@ -17,35 +17,6 @@ require('includes/functions.php');
 <?php echo get_google_snippet('jquery'); ?>
 <script type="text/javascript">
 $(document).ready(function() {
-$('*').css({
-	"-webkit-user-select": "none",
-	"-khtml-user-select": "none",
-	"-moz-user-select": "none",
-	"-o-user-select": "none",
-	"-ms-user-select": "none",
-	"user-select": "none"
-});
-$('body').css({
-	"background-color": "#5fc297",
-	overflow: "none"
-});
-
-$('div#wrapper').css({
-	position: "absolute",
-	left: "50%"
-});
-
-$('div#open').css({
-	color: "#ffffff",
-	"font-size": "40pt",
-	"font-family": "Helvetica Neue, Helvetica, Arial",
-	position: "relative",
-	left: "-50%",
-	"text-align": "center",
-	height: $(window).height(),
-	"line-height": $(window).height() + "px"
-});
-
 /* Get the time */
 var dateinfo = new Date();
 var h = dateinfo.getHours();
@@ -53,13 +24,15 @@ var m = dateinfo.getMinutes();
 var day = dateinfo.getDate();
 var month = dateinfo.getMonth() + 1;
 var year = dateinfo.getFullYear();
+// Ensure time values are two digits long
 if (h < 10) {
 	h = "0" + h;
 }
 if (m < 10) {
 	m = "0" + m;
 }
-var currentTime = h + "" + m; //Format time for the form
+// Format time for form submit
+var currentTime = h + "" + m;
 
 /* Pass the info to the analyzer */
 $.ajax({
@@ -71,22 +44,15 @@ $.ajax({
 		"year": year
 	}
 }).done(function(msg) {
+	// Redirect to the map
 	window.location.href = "map.php";
 });
 });
 </script>
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-45199951-1', 'scyberia.org');
-ga('send', 'pageview');
-</script>
 </head>
 <body>
 <div id="wrapper">
-<div id="open">Working...</div>
+<div id="loading">Working...</div>
 </div>
 </body>
 </html>

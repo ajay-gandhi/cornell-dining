d0dfc89d9221ddb0637c4710e203f7bf0cc24b8c
Ajay Gandhi <aag255@cornell.edu>
2014-11-04 18:08:58 -0500

    Update to reflect changes to Eatery module

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-var request = require('request'),
-    Promise = require('es6-promise').Promise,
-    em      = require('./eatery'),
-    server  = require('./server');
+var request  = require('request'),
+    Promise  = require('es6-promise').Promise,
+    Eateries = require('./eatery'),
+    server   = require('./server');
 
-var d2c = new Date('2014-10-26T20:00:00.000Z');
+var em = new Eateries();
 
-// All the open halls
 console.log('Getting dining hall information...');
-em.are_open(d2c, true).then(function (which_open) {
-
-  // Everything is ready, start the server
-  server.start_server({
-    which_open: which_open
-  });
-  console.log('Server running on port 8080!');
+em.init().then(function (em_initialized) {
+  console.log('Done.');
+  // The eatery module object is initialized
+  // Pass the object to the server module and start this party!
+  server.start_server(em_initialized);
 });

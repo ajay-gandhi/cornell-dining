67a14ee4f3cc4ff64b584b7c3e9c004822fd14d8
Ajay Gandhi <aag255@cornell.edu>
2014-11-14 12:56:48 -0500

    Remove default local server IP

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ module.exports.start_server = function(eatery_object) {
 
   // Actually start the server
   var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-  var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
   app.listen(server_port, server_ip_address, function() {
     console.log('Server running on port ' + server_port);
   });
